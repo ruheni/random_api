702280eb94942620ee0b4a80dc9ad624b37e5234
Ruheni Alex <aleruheni@outlook.com>
2019-02-08 14:28:06 +0300

    removed unused css
    added media queries
    
    appended content from randomuser.me/api to #app of html
    
    added .gitignore file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const app = document.getElementById('app');
 const url = 'https://randomuser.me/api/?results=30';
 
 (async function displayUsers() {
@@ -8,9 +9,43 @@ const url = 'https://randomuser.me/api/?results=30';
 				let first = result.name.first,
 					last = result.name.last,
 					age = result.dob.age,
-					picture = result.picture.thumbnail,
-					email = result.email;
-				console.log({ first, last, age, picture, email });
+					picture = result.picture.large,
+					email = result.email,
+					phone = result.phone;
+
+				let card = document.createElement('div');
+				card.setAttribute('class', 'card');
+
+				let cardContent = document.createElement('div');
+				cardContent.setAttribute('class', 'card-content');
+				card.appendChild(cardContent);
+
+				let cardImg = document.createElement('div');
+				cardImg.setAttribute('class', 'card-image');
+				cardImg.setAttribute(
+					'style',
+					`background-image: url(${picture})`
+				);
+				cardContent.appendChild(cardImg);
+
+				let details = document.createElement('p');
+				let pName = document.createElement('p');
+				pName.append(`Name: ${(last, first)}`);
+				let pAge = document.createElement('p');
+				pAge.append(`Age: ${age}`);
+				let pEmail = document.createElement('p');
+				pEmail.append(`E-mail: ${email}`);
+				let pPhone = document.createElement('p');
+				pPhone.append(`Phone: ${phone}`);
+				details
+					.appendChild(pName)
+					.appendChild(pAge)
+					.appendChild(pEmail)
+					.appendChild(pPhone);
+				cardContent.appendChild(details);
+				app.appendChild(card);
+
+				// console.log({ first, last, age, picture, email, phone });
 			});
 		})
 		.catch(err => console.log(`Oops, Something went wrong ${err}`));
